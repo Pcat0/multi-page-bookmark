281a7d092e0e6a03bb882b6cbd65aff4d46d4629
Patrick Ambli <patrick.ambli@gmail.com>
2019-09-27 11:54:44 -0500

    Update multibookmark.js

--- multibookmark.js
+++ multibookmark.js
@@ -1,7 +1,6 @@
-var thispage;
 function main(query){
     var pages = query.getAll("page");
-    thispage = pages.shift();
+    var thispage = pages.shift();
     var url = window.location.origin + window.location.pathname;
     if (pages.length > 0) {
         var newQuery = new URLSearchParams();
@@ -13,10 +12,6 @@ function main(query){
         loadPage(thispage);
     }
     
-}
-function test(){
-    console.log("test");
-    loadPage(thispage);
 }
 function loadPage(url) {
     this.location.href = url;
